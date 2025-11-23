78d1918712f2f09322a910ceea03419e0831f7a2
VIKAS DUBEY <founder.isekaii@gmail.com>
2025-11-23 11:17:25 +0000

    well , now the logo image i have just provided with same name and same p

--- src/components/app/logo.tsx
+++ src/components/app/logo.tsx
@@ -1,9 +1,10 @@
+
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 export const Logo = ({ className }: { className?: string }) => (
     <Image 
-        src="/images/logo.png" 
+        src="/images/logo.png?v=2" 
         alt="Vikhyat Foundation Logo" 
         width={100} 
         height={100} 
